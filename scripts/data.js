140caccf7d932c1acc31923d1550b28a267765cb
Ofek Basson <107953714+OfekBasson@users.noreply.github.com>
2025-08-29 09:32:36 +0300

    Updated Ofek's URL to the correct new one

--- scripts/data.js
+++ scripts/data.js
@@ -65,7 +65,7 @@ const LAB_DATA = {
       degree: "M.Sc.",
       advisor: "Yacov Hel-Or & Ohad Fried",
       image:
-        "images/people/ofek_basson.PNG",
+        "images/people/ofek_basson.jpg",
       description: "My research lies at the intersection of image generation and 3D understanding, focusing on improving how generative models capture and synthesize the structure of visual scenes.",
       website: "https://github.com/OfekBasson",
       linkedin: "https://www.linkedin.com/in/ofek-basson/",
