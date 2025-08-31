44584e2985e43fcc47140075348b97d1dd75c484
Oron Nir <oronnir11@gmail.com>
2025-08-31 11:26:12 +0300

    Add CAST project information to data.js
    Added new research paper details for CAST project.

--- scripts/data.js
+++ scripts/data.js
@@ -421,5 +421,18 @@ const LAB_DATA = {
         "Translating spoken languages into Sign languages is necessary for open communication between the hearing and hearing-impaired communities. To achieve this goal, we propose the first method for animating a text written in HamNoSys, a lexical Sign language notation, into signed pose sequences. As HamNoSys is universal by design, our proposed method offers a generic solution invariant to the target Sign language. Our method gradually generates pose predictions using transformer encoders that create meaningful representations of the text and poses while considering their spatial and temporal information. We use weak supervision for the training process and show that our method succeeds in learning from partial and inaccurate data. Additionally, we offer a new distance measurement that considers missing keypoints, to measure the distance between pose sequences using DTW-MJE. We validate its correctness using AUTSL, a large-scale Sign language dataset, show that it measures the distance between pose sequences more accurately than existing measurements, and use it to assess the quality of our generated pose sequences. Code for the data pre-processing, the model, and the distance measurement is publicly released for future research.",
       ],
     },
+    {
+      title: "CAST: Character labeling in Animation using Self-supervision by Tracking",
+      authors: ["Oron Nir", "Gal Rapoport", "Ariel Shamir"],
+      conference: "EuroGraphics",
+      date: "25-04-2022",
+      image_link: "https://github.com/oronnir/CAST/raw/master/figures/RL_flow.png",
+      paper_link: "https://arxiv.org/abs/2201.07619",
+      project_link: "https://github.com/oronnir/CAST", // optional project URL
+      abstract: [
+        "Cartoons and animation domain videos have very different characteristics compared to real-life images and videos. In addition, this domain carries a large variability in styles. Current computer vision and deep-learning solutions often fail on animated content because they were trained on natural images. In this paper we present a method to refine a semantic representation suitable for specific animated content.",
+  	    "We first train a neural network on a large-scale set of animation videos and use the mapping to deep features as an embedding space. Next, we use self-supervision to refine the representation for any specific animation style by gathering many examples of animated characters in this style, using a multi-object tracking. These examples are used to define triplets for contrastive loss training. The refined semantic space allows better clustering of animated characters even when they have diverse manifestations. Using this space we can build dictionaries of characters in an animation videos, and define specialized classifiers for specific stylistic content (e.g., characters in a specific animation series) with very little user effort. These classifiers are the basis for automatically labeling characters in animation videos. We present results on a collection of characters in a variety of animation styles."
+      ],
+  },
   ],
 };
